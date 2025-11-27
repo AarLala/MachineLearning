ba4237d533c49bdbda48f0ff1e6f6cc4f4a3c45f
AarLala <aaravh.lala@gmail.com>
2025-11-26 18:51:15 -0600

    Fixed syntax

--- NeuralNetworks/CNN/ConvulationalNN.py
+++ NeuralNetworks/CNN/ConvulationalNN.py
@@ -3,7 +3,7 @@ import torch
 
 def generate_model():
     model = nn.Sequential([
-        torch.nn.Conv2d(in_channels=3, out_channels = 32, kernel_size=3)
+        torch.nn.Conv2d(in_channels=3, out_channels = 32, kernel_size=3),
         torch.nn.ReLU(),
         #reduces the spacial dimension. The pooling layers looks at each 2x2 blocks of the feature map. It will take the max in that 2x2
         torch.nn.MaxPool2d(kernel_size=2,stride=2),
